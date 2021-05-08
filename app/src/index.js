2c07448f584d7a64af34144586044d62e4bf582b
Roald Nefs <info@roaldnefs.com>
2021-05-08 20:48:22 +0200

    feat: add initial dapp
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

---
+++ app/src/index.js
@@ -0,0 +1,51 @@
+import Web3 from "web3";
+import flightSuretyAppArtifact from "../../build/contracts/FlightSuretyApp.json";
+
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
+
+  start: async function() {
+    const { web3 } = this;
+
+    try {
+      // get contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = flightSuretyAppArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(
+        flightSuretyAppArtifact.abi,
+        deployedNetwork.address,
+      );
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+
+      this.refreshBalance();
+    } catch (error) {
+      console.error("Could not connect to contract or chain.");
+    }
+  },
+
+};
+
+window.App = App;
+
+window.addEventListener("load", function() {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn(
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+    );
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+    );
+  }
+
+  App.start();
+});
