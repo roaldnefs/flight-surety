ec45747ab0bc5da12e1159dff94bdbbb35cbb97c
Roald Nefs <info@roaldnefs.com>
2021-05-09 23:03:59 +0200

    feat: show contract status in dapp
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,7 @@ const App = {
     const { web3 } = this;
 
     try {
-      // get contract instance
+      // Get contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = flightSuretyAppArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
@@ -18,13 +18,69 @@ const App = {
         deployedNetwork.address,
       );
 
-      // get accounts
+      // Get accounts.
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
 
-      this.refreshBalance();
+      // Refresh the operational status of the contract at the moment the
+      // dapp is opened.
+      this.refreshStatus();
     } catch (error) {
       console.error("Could not connect to contract or chain.");
+
+      // FIXME: make it DRY
+      // Status elements showing the operational status of the contracts.
+      const activeElements = document.getElementsByClassName("status-active");
+      const inactiveElements = document.getElementsByClassName("status-inactive");
+      const unknownElements = document.getElementsByClassName("status-unknown");
+
+      // Hide the unknown status at the moment we are able to retrieve the
+      // contract status.
+      [...unknownElements].forEach(element => {
+        element.classList.remove("hidden");
+      });
+      [...activeElements].forEach(element => {
+        element.classList.add("hidden");
+      });
+      [...inactiveElements].forEach(element => {
+        element.classList.add("hidden");
+      });
+    }
+  },
+
+  // Refresh the operational status of the contract.
+  refreshStatus: async function() {
+    // Retrieve the operational status of the contracts.
+    const { isOperational } = this.meta.methods;
+    const status = await isOperational().call();
+
+    // Status elements showing the operational status of the contracts.
+    const activeElements = document.getElementsByClassName("status-active");
+    const inactiveElements = document.getElementsByClassName("status-inactive");
+    const unknownElements = document.getElementsByClassName("status-unknown");
+
+    // Hide the unknown status at the moment we are able to retrieve the
+    // contract status.
+    [...unknownElements].forEach(element => {
+      element.classList.add("hidden");
+    });
+
+    // Show the active or inactive status based upon the operational status of
+    // the contracts.
+    if (status) {
+      [...activeElements].forEach(element => {
+        element.classList.remove("hidden");
+      });
+      [...inactiveElements].forEach(element => {
+        element.classList.add("hidden");
+      });
+    } else {
+      [...activeElements].forEach(element => {
+        element.classList.add("hidden");
+      });
+      [...inactiveElements].forEach(element => {
+        element.classList.remove("hidden");
+      });
     }
   },
 
@@ -37,6 +93,11 @@ window.addEventListener("load", function() {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
+
+    window.ethereum.on('networkChanged', (networkId) => {
+      console.log("network changed to " + networkId);
+      App.start();
+    });
   } else {
     console.warn(
       "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
--- test/flightSurety.js
+++ test/flightSurety.js
@@ -1,9 +1,34 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
 
 contract("FlightSuretyApp", accounts => {
-    it("should be testable", async () => {
-      const instance = await FlightSuretyApp.deployed();
-    //   const balance = await instance.getBalance.call(accounts[0]);
-      assert.equal(true, true, "isn't testable");
+
+    it("(multiparty) has correct initial isOperational() value", async () => {
+        const instance = await FlightSuretyApp.deployed();
+        const status = await instance.isOperational.call();
+        assert.equal(status, true, "Incorrect initial operating status value");
     });
+
+    it("(multiparty) can block access to setOperatingStatus() for non-contract owner account", async () => {
+        const instance = await FlightSuretyData.deployed();
+        let denied = false;
+        try {
+            await instance.setOperatingStatus(false, {from: accounts[1]});
+        } catch(error) {
+            denied = true;
+        }
+        assert.equal(denied, true, "Access not restricted to contract owner");
+    });
+
+    it("(multiparty) can allow access to setOperatingStatus() for contract owner account", async () => {
+        const instance = await FlightSuretyData.deployed();
+        let denied = false;
+        try {
+            await instance.setOperatingStatus(false, {from: accounts[0]});
+        } catch(error) {
+            denied = true;
+        }
+        assert.equal(denied, false, "Access not restricted to contract owner");
+    });
+
   });
